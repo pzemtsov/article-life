4819ad053b8ef7b86163e48522bff99c052b3468
Pavel Zemtsov <pzemtsov@yahoo.com>
2015-12-02 12:45:56 +0200

    ArrayList of keys replaced with ArrayList of Entry objects

--- Life.java
+++ Life.java
@@ -81,6 +81,7 @@ public class Life
 //      test (new Hash1 (LongPoint6.factory));
 //      test (new Hash2 (LongPoint6.factory));
 //      test (new Hash3 (LongPoint6.factory));
-        test (new Hash4 (LongPoint6.factory));
+//      test (new Hash4 (LongPoint6.factory));
+        test (new Hash5 (LongPoint6.factory));
     }
 }
